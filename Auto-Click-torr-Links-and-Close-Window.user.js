b70ba078d18881c3a3b828c12fbcde33677f8eb6
emukus <101443265+emukus@users.noreply.github.com>
2023-10-12 11:04:26 +0100

    Update Auto-Click-torr-Links-and-Close-Window.user.js

--- Auto-Click-torr-Links-and-Close-Window.user.js
+++ Auto-Click-torr-Links-and-Close-Window.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Auto Click .torrent Links and Close Window
 // @namespace    http://github.com/emukus
-// @version      1.1
-// @description  Automatically click on links ending with .torrent and close the window if a torrent link is found
+// @version      1.2
+// @description  Automatically click on links ending with .torrent and close the window if a torrent link is found after four cycles
 // @match        https://get-to.link/*
 // @match        https://www.spaste.com/*
 // @author       emukus
@@ -13,9 +13,9 @@
 (function() {
     'use strict';
 
-    // Function to check if a link ends with .torrent
+    // Function to check if a link ends with .torrent (with optional query parameters)
     function endsWithTorrent(link) {
-        return link.endsWith('.torrent');
+        return /\.torrent(\?.*)?$/.test(link);
     }
 
     // Function to close the window
@@ -23,21 +23,27 @@
         window.close();
     }
 
-    // Find all links on the page
-    const links = document.querySelectorAll('a');
+    // Function to periodically check for the torrent link
+    function checkForTorrentLink(cycles) {
+        const links = document.querySelectorAll('a');
+        let hasTorrentLink = false;
 
-    let hasTorrentLink = false;
+        // Iterate through the links and check if any end with .torrent
+        links.forEach(link => {
+            if (endsWithTorrent(link.href)) {
+                link.click();
+                hasTorrentLink = true;
+            }
+        });
 
-    // Iterate through the links and click on the ones ending with .torrent
-    links.forEach(link => {
-        if (endsWithTorrent(link.href)) {
-            link.click();
-            hasTorrentLink = true;
+        // Schedule window closure if a torrent link is found or continue checking
+        if (hasTorrentLink) {
+            setTimeout(closeWindow, 8500); // Close after 8.5 seconds
+        } else if (cycles > 0) {
+            setTimeout(() => checkForTorrentLink(cycles - 1), 25000); // Check again in 25 seconds
         }
-    });
-
-    // Schedule window closure if at least one torrent link is found
-    if (hasTorrentLink) {
-        setTimeout(closeWindow, 8500); // 8000 milliseconds = 8 seconds
     }
+
+    // Start the checking process with 4 cycles
+    checkForTorrentLink(4);
 })();
